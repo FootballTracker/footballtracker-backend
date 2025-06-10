59283d8c7e2cbe769654d4f1a21fe47827abbb84
Talles <talves2503@gmail.com>
2025-06-10 16:59:56 -0300

    feat: add cors setup

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import (
     auth,
     fetch_countries,
@@ -16,9 +17,21 @@ from routes import (
     user_image
 )
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
 
 app = FastAPI()
 
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=['*'],
+  allow_headers=['*']
+)
 
 app.include_router(auth.router)
 app.include_router(leagues.router)
